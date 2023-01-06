50bd2b126a4d07d189e53c58dde2a99101d0cb15
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-06 15:19:04 +0530

    Plus button add for add button with animation

--- MyHabitApp/View/HomeView.swift
+++ MyHabitApp/View/HomeView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            
+        ZStack{
+            VStack{
+                ScrollView(.vertical, showsIndicators: false) {
+                 
+                }
+                .safeAreaInset(edge: .top, spacing: 0) {
+                    HeaderView()
+                    
+                }
+        AddTaskView1()
+            }
         }
-        .safeAreaInset(edge: .top, spacing: 0) {
-            HeaderView()
-        }
+        
     }
     
     /// - Header View
@@ -39,6 +46,55 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+// MARK: Add Plus Button View
+struct AddTaskView1: View {
+  @State var showCreateTaskView = false
+    @State private var animate: Bool = false
+    private let secondaryAccentColor = Color(.blue)
+
+  var body: some View {
+    VStack {
+      Spacer()
+      HStack {
+        Spacer()
+        Button(
+          action: {
+          //  showCreateTaskView = true
+          }, label: {
+           Image(systemName: "plus")
+                  .resizable()
+              .multilineTextAlignment(.center)
+              .frame(width: 20, height: 20)
+              .foregroundColor(Color.white)
+              .padding()
+          })
+        .background(animate ? Color.purple : Color.blue)
+          .cornerRadius(20)
+          .padding()
+          .padding(.horizontal, animate ? 30 : 30)
+          .scaleEffect(animate ? 1.1 : 1.0)
+          .offset(y: animate ? -1 : .zero)
+//          .sheet(isPresented: $showCreateTaskView) {
+//            CreateTaskView()
+//          }
+      }
+      .padding(.bottom)
+    }
+    .onAppear(perform: addAnimation)
+  }
+    private func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                self.animate.toggle()
+            }
+        })
     }
 }
 
