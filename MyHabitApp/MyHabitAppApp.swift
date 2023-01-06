dda28f1d1688c795edcaa2fa7d7de9869c9e851b
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-01-06 12:17:57 +0530

    Initial Commit

---
+++ MyHabitApp/MyHabitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitAppApp.swift
+//  MyHabitApp
+//
+//  Created by Bhumika Patel on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
